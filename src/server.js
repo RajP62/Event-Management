be67023359ea1a7492eebe36c9d9fc3b7115aa5a
RajP62 <rajesh4151999@gmail.com>
2022-05-19 15:05:27 +0530

    new

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app=require("./index");
 const connect=require("./configs/db");
-app.listen(2345, async()=>{
+app.listen(process.env.PORT || 2345, async()=>{
     try {
         await connect();
         console.log("Listening on port 2345.");
